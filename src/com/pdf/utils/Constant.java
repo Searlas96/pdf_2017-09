18db2a158ff6ecb2e43d019b71c4f7b997151c4a
searlas <sqf2009@vip.qq.com>
2018-06-04 15:14:51 +0800

    modi

--- src/com/pdf/utils/Constant.java
+++ src/com/pdf/utils/Constant.java
@@ -1,7 +1,8 @@
 package com.pdf.utils;
 
 public class Constant {
+
 	public static String PdfLocation = "D:/pdf/project/";
 //	public static String PdfLocation = "C:/pdf/project/";
-//	public static String PdfLocation = "/home/user/opt/pdf/project/";
+	public static String PdfLocation2 = "/home/user/opt/pdf/project/";
 }
